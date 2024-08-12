33a7fec492739b98e3854ea721b25a304b4e6110
Shay D. <dardenteysha@gmail.com>
2024-08-12 12:07:26 -0500

    Put my Javascript back to its original format.

--- src/index.js
+++ src/index.js
@@ -1,41 +1,28 @@
-document.addEventListener("DOMContentLoaded", function () {
-  function displayJoke(response) {
-    let jokeElement = document.querySelector("#joke");
-    jokeElement.classList.remove("joke-loaded");
-    jokeElement.innerHTML = response.data.answer;
+function displayJoke(response) {
+  let jokeElement = document.querySelector("#joke");
+  jokeElement.innerHTML = response.data.answer;
 
-    new Typewriter("#joke", {
-      strings: response.data.answer,
-      autoStart: true,
-      cursor: null,
-      delay: 20,
-    });
+  new Typewriter("#joke", {
+    strings: response.data.answer,
+    autoStart: true,
+    cursor: null,
+    delay: 20,
+  });
+}
 
-    setTimeout(() => {
-      jokeElement.classList.add("joke-loaded");
-    }, 50);
-  }
+function generateJoke(event) {
+  event.preventDefault();
+  let apiKey = "a43ab0c5t7640e69ab4693092fo45cea";
+  let context =
+    "You are a funny AI Assistant that tells short and funny jokes. The joke must be provided in HTML format. Example: <p>This is a joke</p>";
+  let prompt = "Generate a short and unique joke to make people laugh.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  function generateJoke(event) {
-    event.preventDefault();
-    let generatorButton = document.querySelector("#generate-joke-button");
-    generatorButton.disabled = true;
+  let jokeElement = document.querySelector("#joke");
+  jokeElement.innerHTML = "Loading.... please wait";
 
-    let apiKey = "a43ab0c5t7640e69ab4693092fo45cea";
-    let context =
-      "You are a funny AI Assistant that tells short and funny jokes. The joke must be provided in HTML format. Example: <p>This is a joke</p>";
-    let prompt = "Generate a short and unique joke to make people laugh.";
-    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayJoke);
+}
 
-    let jokeElement = document.querySelector("#joke");
-    jokeElement.innerHTML = "Loading.... please wait";
-
-    axios.get(apiUrl).then((response) => {
-      displayJoke(response);
-      generatorButton.disabled = false;
-    });
-  }
-
-  let generatorButton = document.querySelector("#generate-joke-button");
-  generatorButton.addEventListener("click", generateJoke);
-});
+let generatorButton = document.querySelector("#generate-joke-button");
+generatorButton.addEventListener("click", generateJoke);
