bc0b29db3129dd2458ea30e6acc1b58ea747fe33
Shay D. <dardenteysha@gmail.com>
2024-08-12 11:57:06 -0500

    Javascipt code updated.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 function displayJoke(response) {
   let jokeElement = document.querySelector("#joke");
+  jokeElement.classList.remove("joke-loaded");
   jokeElement.innerHTML = response.data.answer;
 
   new Typewriter("#joke", {
@@ -8,10 +9,17 @@ function displayJoke(response) {
     cursor: null,
     delay: 20,
   });
+
+  setTimeout(() => {
+    jokeElement.classList.add("joke-loaded");
+  }, 50);
 }
 
 function generateJoke(event) {
   event.preventDefault();
+  let generatorButton = document.querySelector("#generate-joke-button");
+  generatorButton.disabled = true;
+
   let apiKey = "a43ab0c5t7640e69ab4693092fo45cea";
   let context =
     "You are a funny AI Assistant that tells short and funny jokes. The joke must be provided in HTML format. Example: <p>This is a joke</p>";
@@ -21,7 +29,10 @@ function generateJoke(event) {
   let jokeElement = document.querySelector("#joke");
   jokeElement.innerHTML = "Loading.... please wait";
 
-  axios.get(apiUrl).then(displayJoke);
+  axios.get(apiUrl).then((response) => {
+    displayJoke(response);
+    generatorButton.disabled = false;
+  });
 }
 
 let generatorButton = document.querySelector("#generate-joke-button");
